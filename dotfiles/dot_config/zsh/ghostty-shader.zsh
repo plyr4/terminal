c29e260d4253ebd9e3c5110b06e57e03650ac7c1
plyr4 <davelvader@gmail.com>
2026-08-11 13:45:07 -0500

    fix: shader toggle race condition

--- dotfiles/dot_config/zsh/ghostty-shader.zsh
+++ dotfiles/dot_config/zsh/ghostty-shader.zsh
@@ -10,8 +10,10 @@ if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
 	typeset -gi _GC_COUNTER_MOD=$(( _GC_WRAP_SLOTS * _GC_STEPS ))
 	typeset -gi _GC_COUNTER=0
 	typeset -gi _GC_ENABLED=1
+	typeset -gi _GC_GEN=0
 
 	function _gc_emit_counter() {
+		(( _GC_ENABLED )) || return 0
 		local counter=$(( $1 % _GC_COUNTER_MOD ))
 		local high=$(( counter / 256 ))
 		local hi=$(( (counter % 256) / 16 ))
@@ -25,17 +27,21 @@ if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
 	}
 
 	function _gc_roll() {
-		local -i start=$1 i
+		local -i start=$1 gen=$2 i
 		for (( i = 1; i <= _GC_STEPS; i++ )); do
+			(( gen == _GC_GEN )) || break
 			zselect -t $_GC_STEP_CS
+			(( gen == _GC_GEN )) || break
 			_gc_emit_counter $(( (start + i) % _GC_COUNTER_MOD ))
 		done
 	}
 
 	function _gc_roll_reverse() {
-		local -i start=$1 i
+		local -i start=$1 gen=$2 i
 		for (( i = 1; i <= _GC_STEPS; i++ )); do
+			(( gen == _GC_GEN )) || break
 			zselect -t $_GC_STEP_CS
+			(( gen == _GC_GEN )) || break
 			_gc_emit_counter $(( (start - i + _GC_COUNTER_MOD) % _GC_COUNTER_MOD ))
 		done
 	}
@@ -45,8 +51,9 @@ if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
 		if (( now >= _GC_NEXT_ALLOWED )); then
 			(( _GC_NEXT_ALLOWED = now + _GC_ROLL_DURATION ))
 			local -i start=$_GC_COUNTER
+			local -i gen=$_GC_GEN
 			(( _GC_COUNTER = (_GC_COUNTER + _GC_STEPS) % _GC_COUNTER_MOD ))
-			_gc_roll $start &!
+			_gc_roll $start $gen &!
 		fi
 		zle .self-insert "$@"
 	}
@@ -56,14 +63,16 @@ if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
 		if (( now >= _GC_NEXT_ALLOWED )); then
 			(( _GC_NEXT_ALLOWED = now + _GC_ROLL_DURATION ))
 			local -i start=$_GC_COUNTER
+			local -i gen=$_GC_GEN
 			(( _GC_COUNTER = (_GC_COUNTER - _GC_STEPS + _GC_COUNTER_MOD) % _GC_COUNTER_MOD ))
-			_gc_roll_reverse $start &!
+			_gc_roll_reverse $start $gen &!
 		fi
 		zle .backward-delete-char "$@"
 	}
 
 	function _gc_enable() {
 		(( _GC_ENABLED = 1 ))
+		(( _GC_GEN++ ))
 		zle -N self-insert _gc_self_insert
 		zle -N backward-delete-char _gc_backspace
 		_gc_emit_counter $_GC_COUNTER
@@ -71,6 +80,7 @@ if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
 
 	function _gc_disable() {
 		(( _GC_ENABLED = 0 ))
+		(( _GC_GEN++ ))
 		zle -A .self-insert self-insert
 		zle -A .backward-delete-char backward-delete-char
 		printf '\033]112\a' > "$TTY" 2>/dev/null
@@ -84,7 +94,7 @@ if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
 		fi
 	}
 
-	function shader() {
+	function toggle-shader() {
 		_gc_toggle
 	}
 
